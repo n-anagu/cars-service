e3ca9ba658dbbbba4734bab9c5f6be30ac97b209
Natalia Gurgul <natalia.gurgul@wolterskluwer.com>
2020-01-19 19:25:06 +0100

    Changed interface for car data

--- src/app/cars/models/car.ts
+++ src/app/cars/models/car.ts
@@ -1,12 +1,15 @@
-import { Client } from './client';
-
 export interface Car {
-  id: number;
-  model: string;
-  plate: string;
-  deliveryDate: string;
-  deadline: string;
-  client: Client;
-  cost: number;
-  isFullyDamaged: boolean;
+  id: number,
+  model: string,
+  type: string,
+  year: string,
+  color: string,
+  cost: number,
+  isFullyDamaged: false,
+  clientFirstName: string,
+  clientSurname: string,
+  power: number,
+  plate: string,
+  deliveryDate: string,
+  deadline: string,
 }
