da24db59844b55d8b96475c4182a602649a21417
Natalia Gurgul <natalia.gurgul@wolterskluwer.com>
2020-01-19 21:35:52 +0100

    Added shared module with header component

--- src/app/cars/cars.module.ts
+++ src/app/cars/cars.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarsListComponent } from './cars-list/cars-list.component';
 import { TotalCostComponent } from './total-cost/total-cost.component';
+import { SharedModule } from '../shared-module/shared.module';
 
 @NgModule({
   declarations: [CarsListComponent, TotalCostComponent],
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule, SharedModule],
   exports: [CarsListComponent],
 })
+
 export class CarsModule {}
