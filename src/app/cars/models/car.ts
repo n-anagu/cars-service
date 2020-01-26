45a53058c6d953e2e1f78547efd66802be8758a8
Natalia Gurgul <natalia.gurgul@wolterskluwer.com>
2020-01-26 22:13:22 +0100

    Lint fix - semicolons

--- src/app/cars/models/car.ts
+++ src/app/cars/models/car.ts
@@ -1,15 +1,15 @@
 export interface Car {
-  id: number,
-  model: string,
-  type: string,
-  year: string,
-  color: string,
-  cost: number,
-  isFullyDamaged: false,
-  clientFirstName: string,
-  clientSurname: string,
-  power: number,
-  plate: string,
-  deliveryDate: string,
-  deadline: string,
+  id: number;
+  model: string;
+  type: string;
+  year: string;
+  color: string;
+  cost: number;
+  isFullyDamaged: false;
+  clientFirstName: string;
+  clientSurname: string;
+  power: number;
+  plate: string;
+  deliveryDate: string;
+  deadline: string;
 }
