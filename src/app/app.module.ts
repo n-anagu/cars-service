7b1e98b0fb95dcf54a9cd095131daa34c53c31c0
Natalia Gurgul <natalia.gurgul@wolterskluwer.com>
2020-01-26 19:51:26 +0100

    Added core module with sidebar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarsModule } from './cars/cars.module';
 import { HttpClientModule } from '@angular/common/http';
+import { CoreModule } from './core-module/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule, CarsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    CarsModule,
+    CoreModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule {}
